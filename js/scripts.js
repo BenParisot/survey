35d06730d2d4b56b00c1384e8bb0c406955e1a65
Ben Parisot <me@benparisot.com>
2017-10-18 17:07:21 -0700

    Update html and script so that jumbotron also disappears on submit

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
 
 
 
-	$("#blanks").submit(function() {
-		$("#blanks").fadeOut( 1600, "linear", function() {
+	$("#input").submit(function() {
+		$("#input").fadeOut( 1600, "linear", function() {
 				$("#result").fadeIn(1600, "linear" );
 		});
 		event.preventDefault();
